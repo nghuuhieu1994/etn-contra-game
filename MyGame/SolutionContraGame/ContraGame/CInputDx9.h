5cb05ac4ec8d30a8440749b52cba14c99ad9dec3
nghuuhieu1994 <nghuuhieu1994@7750864d-e774-df23-76f5-1805eb8f8076>
2014-09-09 13:57:58 +0000

    [Hieu] commit Input class

---
+++ MyGame/SolutionContraGame/ContraGame/CInputDx9.h
@@ -0,0 +1,53 @@
+#ifndef __CINPUT_H__
+#define __CINPUT_H__
+#include <dinput.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include "GameLog.h"
+
+
+class CInputDx9
+{
+	//Manage Input of Game
+	LPDIRECTINPUT8			m_lpDirectInput;
+	
+	LPDIRECTINPUTDEVICE8	m_lpKeyBoardDevice;
+	
+	LPDIRECTINPUTDEVICE8	m_lpMouseDevice;
+
+	DIMOUSESTATE2			m_mouseState;
+
+	D3DXVECTOR2				m_cursorLocation;
+
+	char					m_keyBoardBuffer[256];
+
+	int						m_lastKeyDown;
+
+	int						m_currentKeyDown;
+	
+public:
+	CInputDx9();
+	void InitializeInput();
+	void InitializeKeyBoardDevice(HWND handleWindow);
+	void InitializeMouseDevice(HWND handleWindow);
+
+	void UpdateMouse();
+
+	void UpdateKeyBoard();
+
+	D3DXVECTOR2 GetCursorLocation();
+
+	bool IsKeyDown(int keyCode);
+
+	bool IsKeyUp(int keyCode);
+
+	bool IsKeyPress(int keyCode);
+
+	bool IsMouseRightClick();
+
+	bool IsMouseLeftClick();
+
+	~CInputDx9();
+};
+
+#endif
