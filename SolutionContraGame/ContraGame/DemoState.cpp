ff63726604a5fcec161c7f2c5eac05f14bcfc24e
huudanh.k7 <huudanh.k7@7750864d-e774-df23-76f5-1805eb8f8076>
2014-11-22 15:15:12 +0000

    - Trânf Hữu Danh đẹp trai nhất ETN commit lần cuối liên quan đến fps. Kể từ sau này cái gì làm tụt fps thì TrầnDanh hông chiệu trách nhiệm đâu >_<

--- SolutionContraGame/ContraGame/DemoState.cpp
+++ SolutionContraGame/ContraGame/DemoState.cpp
@@ -16,36 +16,16 @@ void DemoState::InitializeState(LPDIRECT3DDEVICE9 _lpDirectDevice)
 
 void DemoState::HandleInput()
 {
-	m_Rambo->HandleInput();
 }
 
 void DemoState::Update()
 {
-	Camera::getInstance()->UpdateCamera(&m_Rambo->getPositionVec3());
-	m_Quadtree->InsertObjectIntoView(Camera::getInstance()->getBound(), m_Quadtree->mRootNode);
-
-	m_Rambo->Update();
-	m_Rambo->UpdateAnimation();
-	m_Rambo->UpdateMovement();
-
-
-
 	m_myTinker->UpdateAnimation();
 }
 
 void DemoState::Render(LPD3DXSPRITE _lpDSpriteHandle)
 {
-	//for(int i = 0; i < (int)m_Quadtree->mListObjectInView.size(); ++i)
-	//{
-	//	m_Quadtree->mListObjectInView[i]->Render(_lpDSpriteHandle);
-	//}
-
-	m_Quadtree->Render(_lpDSpriteHandle);
-	m_Rambo->Render(_lpDSpriteHandle);
-	BulletPoolManager::getInstance()->Render(_lpDSpriteHandle);
-
 	m_myTinker->Render(_lpDSpriteHandle);
-
 }
 
 void DemoState::Pause()
